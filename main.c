b2f309fe252d706f7136e11c6681efba8c290634
wjdwjdwjd1279 <alfzltm9721@naver.com>
2023-11-12 02:10:02 +0900

    10-5

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[30] = "happy C programming";
 	
-	printf("문자열\"%s\"의 길이 : %i \n", src, strlen(src))	;
+	FILE* fp;
+	char str[100];
+	
+	fp =fopen("sample.txt","w");
+	printf("input a word:");
+	scanf("%s", str);
+	fprintf(fp, "%s", str);
+	fclose(fp);
+	
 
 }
